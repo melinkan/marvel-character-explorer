a3ca171df16d69d2a257d2aca8e0541d0305785b
Melin Kan <melin.kan@outlook.com>
2022-08-25 20:25:52 +0300

    Implement infinite scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,45 @@
 import { useEffect, useState } from "react";
 import { Header, Card, CharList } from "./components";
+import axios from "axios";
 
 function App() {
   const [chars, setChars] = useState([]);
+  let offset = 0;
 
-  useEffect(() => {
-    fetch(
-      "https://gateway.marvel.com:443/v1/public/characters?apikey=3ed3143d1f4660857e06b20c20aa1a2a"
-    )
-      .then((data) => data.json())
+  const loadMore = () => {
+    axios
+      .get(
+        `https://gateway.marvel.com:443/v1/public/characters?limit=30&offset=${offset}&apikey=3ed3143d1f4660857e06b20c20aa1a2a`
+      )
       .then(({ data }) => {
-        setChars(data.results);
+        const newChars = [];
+        data.data.results.forEach((char) => newChars.push(char));
+        setChars((prev) => [...prev, ...newChars]);
         console.log(data);
       });
+    offset += 30;
+  };
+
+  const handleScroll = (e) => {
+    if (
+      window.innerHeight + e.target.documentElement.scrollTop + 1 >=
+      e.target.documentElement.scrollHeight
+    ) {
+      loadMore();
+    }
+  };
+
+  useEffect(() => {
+    loadMore();
+    window.addEventListener("scroll", handleScroll);
   }, []);
 
   return (
     <>
       <Header />
       <CharList>
-        {chars.map((char) => {
-          return <Card char={char} />;
+        {chars.map((char, index) => {
+          return <Card key={index} char={char} />;
         })}
       </CharList>
     </>
