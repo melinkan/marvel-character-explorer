d5e169473d767bab7c8ddc31c399df5f34be59b3
Melin Kan <melin.kan@outlook.com>
2022-08-25 10:19:29 +0300

    Implement basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+import { Header, Card, CharList } from "./components";
+
 function App() {
-  return <div>Marvel App</div>;
+  const [chars, setChars] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://gateway.marvel.com:443/v1/public/characters?apikey=3ed3143d1f4660857e06b20c20aa1a2a"
+    )
+      .then((data) => data.json())
+      .then(({ data }) => {
+        setChars(data.results);
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <CharList>
+        {chars.map((char) => {
+          return <Card char={char} />;
+        })}
+      </CharList>
+    </>
+  );
 }
 
 export default App;
