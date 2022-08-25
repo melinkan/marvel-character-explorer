c9ec4003e6b86de3a6ca90b808cb837cd30b177a
Melin Kan <melin.kan@outlook.com>
2022-08-25 10:13:26 +0300

    Create CharList component

---
+++ src/components/CharList/CharList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./CharList.module.scss";
+
+export const CharList = ({ children }) => {
+  return (
+    <main className={styles.base}>
+      <div className={`container ${styles.container}`}>{children}</div>
+    </main>
+  );
+};
