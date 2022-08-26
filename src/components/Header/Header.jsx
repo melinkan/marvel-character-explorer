97b6ee0ee6d429f80f6a3b535f251f9cc58a1498
Melin Kan <melin.kan@outlook.com>
2022-08-26 19:29:56 +0300

    Add link to logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import marvelLogo from "../../assets/marvel-logo.svg";
 import styles from "./Header.module.scss";
 
@@ -6,7 +7,9 @@ export function Header() {
   return (
     <header className={styles.base}>
       <div className={`container ${styles.container}`}>
-        <img src={marvelLogo} alt="logo" />
+        <Link to="/">
+          <img src={marvelLogo} alt="logo" />
+        </Link>
 
         <form action="">
           <input type="text" />
