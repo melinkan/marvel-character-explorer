11de10dab1f742125a12968c5dfe56aca6a07735
Melin Kan <melin.kan@outlook.com>
2022-08-26 19:30:12 +0300

    Add link to card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Card.module.scss";
+import { Link } from "react-router-dom";
 
 export const Card = ({ char }) => {
   return (
@@ -9,6 +10,9 @@ export const Card = ({ char }) => {
       </div>
       <div className={styles.cardtitle}>
         <p>{char.name}</p>
+        <Link to={`/characters/${char.id}`} className={styles.button}>
+          Learn More
+        </Link>
       </div>
     </div>
   );
