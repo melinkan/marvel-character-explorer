951b97bf16ac305d552cb2bf6dd18794a21ed0c1
Melin Kan <melin.kan@outlook.com>
2022-08-26 23:41:51 +0300

    Refactor Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,15 +4,24 @@ import { Link } from "react-router-dom";
 
 export const Card = ({ char }) => {
   return (
-    <div className={styles.cardwrapper}>
-      <div className={styles.cardimg}>
-        <img src={char.thumbnail.path + "." + char.thumbnail.extension} />
-      </div>
-      <div className={styles.cardtitle}>
-        <p>{char.name}</p>
-        <Link to={`/characters/${char.id}`} className={styles.button}>
-          Learn More
-        </Link>
+    <div class={styles.card}>
+      <div class={styles.content}>
+        <div class={styles.front}>
+          <div className={styles.cardimg}>
+            <img
+              className="img-fluid"
+              src={char.thumbnail.path + "." + char.thumbnail.extension}
+            />
+          </div>
+          <div className={styles.cardTitle}>
+            <p>{char.name}</p>
+          </div>
+        </div>
+        <div class={styles.back}>
+          <Link to={`/characters/${char.id}`} className={styles.button}>
+            Learn More
+          </Link>
+        </div>
       </div>
     </div>
   );
